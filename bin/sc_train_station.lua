dee655c979f2a2ea36a55091ba8f46f2636bc4f1
Pixailz <Pixailz1@gmail.com>
2024-01-08 21:18:22 +0100

    station: create class

--- bin/sc_train_station.lua
+++ bin/sc_train_station.lua
@@ -10,112 +10,110 @@ local	periph_im = require("periph-im")
 local	utils = require("utils")
 local	log = require("log")
 
---- ATTRIBUTS
-local	conf_path = nil
-local	conf = nil
+--- UTILS
+function	get_dot(i) return string.rep(".", i)..string.rep(" ", 3 - i) end
 
---- TEST
+--- STATION
 
---- INIT
-if utils.hostname then
-	local	conf_path = "/usr/etc/"..utils.hostname..".lua"
+local	STATION_CONF_PATH = "/usr/etc/"
 
-	conf = loadfile(conf_path)
-	if conf == nil then
-		log.fail("sc_train_station: config file, "..conf_path..", not found")
+station = {}
+
+	--- INIT
+function	station.get_conf()
+	local	conf_path = nil
+	if utils.hostname then
+		STATION_CONF_PATH = STATION_CONF_PATH..utils.hostname..".lua"
+		station.conf = loadfile(STATION_CONF_PATH)
+		if station.conf == nil then
+			log.fail(station.name..": config file, "..STATION_CONF_PATH..", not found")
+		else
+			station.conf = station.conf()
+		end
 	else
-		conf = conf()
+		log.fail(station.name..": hostname not found, aborting.")
 	end
-else
-	log.fail("sc_train_station: hostname not found, aborting.")
+	if station.conf == nil then os.exit(1) end
 end
 
-if conf == nil then os.exit(1) end
+function	station.get_peripheral()
+	for p_k, p_v in pairs(station.conf.platform) do
+		log.info("setup platform "..p_k)
 
-utils.enum_table(conf)
+		for ad_k, ad_v in pairs(p_v.AD) do
+			station.conf.platform[p_k].AD[ad_k] = periph.get(ad_k, ad_v)
+			if not station.conf.platform[p_k].AD[ad_k] then os.exit(1) end
+		end
 
-for p_k, p_v in pairs(conf.platform) do
-	log.info("setup platform "..p_k)
-	for ad_k, ad_v in pairs(p_v.AD) do
-		conf.platform[p_k].AD[ad_k] = periph.get(ad_k, ad_v)
-		if not conf.platform[p_k].AD[ad_k] then
-			os.exit(1)
+		for ac_k, ac_v in pairs(p_v.AC) do
+			station.conf.platform[p_k].AC[ac_k] = periph.get(ac_k, ac_v)
+			if not station.conf.platform[p_k].AC[ac_k] then os.exit(1) end
 		end
 	end
-	for ac_k, ac_v in pairs(p_v.AC) do
-		conf.platform[p_k].AC[ac_k] = periph.get(ac_k, ac_v)
-		if not conf.platform[p_k].AC[ac_k] then
-			os.exit(1)
-		end
+
+	for a_k, a_v in pairs(station.conf.arrival) do
+		log.info("setup arrival "..a_k)
+
+		station.conf.arrival[a_k].AC = periph.get("arrival_"..a_k, a_v.AC)
+		if not station.conf.arrival[a_k].AC then os.exit(1) end
+
+		station.conf.arrival[a_k].AD = periph.get("arrival_"..a_k, a_v.AD)
+		if not station.conf.arrival[a_k].AD then os.exit(1) end
 	end
 end
 
-for a_k, a_v in pairs(conf.arrival) do
-	log.info("setup arrival "..a_k)
-	-- for a_ad_k, a_ad_v in pairs(a_v.AC) do
-	-- 	conf.arrival[a_k].AC[a_ad_k] = periph.get("arrival_"..a_ad_k, a_ad_v)
-	-- end
-	conf.arrival[a_k].AC = periph.get("arrival_"..a_k, a_v.AC)
-	if not conf.arrival[a_k].AC then os.exit(1)
-	end
-	conf.arrival[a_k].AD = periph.get("arrival_"..a_k, a_v.AD)
-	if not conf.arrival[a_k].AD then os.exit(1) end
+function	station.init()
+	station.get_conf(STATION_CONF_PATH)
+	log.info("setup station "..station.conf.name)
+	-- utils.enum_table(station.conf)
+	station.get_peripheral()
 end
 
-function	get_dot(i)
-	return string.rep(".", i)..string.rep(" ", 3 - i)
-end
-
-function	train_arrival(arrival, platform)
-	local	info = nil
-
-	arrival.AD.setTag("01-arrived")
+	--- RUN
+function	station.arrival_brake(arrival, platform_id)
+	arrival.AD.setTag(platform_id.."-arrived")
 	arrival.AC.setThrottle(0)
 	arrival.AC.setBrake(arrival.brake)
+end
 
-	local	waiting_for_train = 1
+function	station.platform_wait_for_arrival(platform, platform_id)
+	local	i = 1
 	local	term_x, term_y = term.getCursor()
+	local	msg = station.name..": Waiting for arrival at "..platform_id
 
+	log.info(msg)
 	while true do
-		log.info("waiting for train"..get_dot(waiting_for_train))
+		print(msg..get_dot(i))
 		term.setCursor(term_x, term_y - 1)
 		info = platform.AD["end"].getTag()
 
-		if info == "01-arrived" then
-			break
-		end
+		if info == platform_id.."-arrived" then break end
 
-		os.sleep(utils.tick)
-		if waiting_for_train < 3 then
-			waiting_for_train = waiting_for_train + 1
+		if i < 3 then
+			i = i + 1
 		else
-			waiting_for_train = 1
+			i = 1
 		end
+		os.sleep(utils.tick)
 	end
 	term.setCursor(term_x, term_y)
-
-	platform.AD["end"].setTag("01-at-station")
+	platform_ad.setTag(platform_id.."-at-station")
+	platform.AD["end"].setTag(platform_id.."-at-station")
 	platform.AC["end"].setBrake(1)
 	platform.AC["deadend"].setBrake(1)
 	platform.AC["end"].setThrottle(0)
 	platform.AC["deadend"].setThrottle(0)
+end
 
-	log.pass("Train successfully arrived at platform 01")
-
-	platform.AC["end"].setBrake(1)
-	platform.AC["deadend"].setBrake(1)
-
-	local	waiting_for_departure = platform.depart_time
+function	station.platform_wait_for_departure(platform, platform_id)
 	local	term_x, term_y = term.getCursor()
+	local	fmt = station.name..": Train departure in %d sec (%d/60)"
+	local	info = nil
 
-	info = platform.AD["end"].info()
-
-	while waiting_for_departure > 0 do
+	for i = platform.depart_time, 1, -1 do
 		info = platform.AD["end"].info()
-		log.info("Train departure in "..waiting_for_departure.." sec ("..info.passengers.."/60)"..get_dot((waiting_for_departure % 3) + 1))
+		print(string.format(fmt, i, info.passengers))
 		term.setCursor(term_x, term_y - 1)
-		waiting_for_departure = waiting_for_departure - 1
-
 		platform.AC["end"].setBrake(1)
 		platform.AC["deadend"].setBrake(1)
 		platform.AC["end"].setThrottle(0)
@@ -125,31 +123,42 @@ function	train_arrival(arrival, platform)
 	term.setCursor(term_x, term_y)
 
 	info = platform.AD["end"].info()
+	platform.AD["end"].setTag("")
 	platform.AC["end"].setThrottle(platform.throttle)
 	platform.AC["deadend"].setThrottle(platform.throttle)
 	platform.AC["end"].setBrake(0)
 	platform.AC["deadend"].setBrake(0)
 	if info.passengers == 1 then
-		log.info("train go off with "..info.passengers.." passenger")
+		log.info(station.name..": Train go off with "..info.passengers.." passenger")
 	else
-		log.info("train go off with "..info.passengers.." passengers")
+		log.info(station.name..": Train go off with "..info.passengers.." passengers")
 	end
 end
 
-running = true
+function	station.train_arrival(arrival, platform_id)
+	local	platform = station.conf.platform[platform_id]
+	local	info = nil
 
-while running do
-	local	info = conf.arrival["01"].AD.info()
-
-	if info then
-		-- utils.enum_table(info)
-		train_arrival(conf.arrival["01"], conf.platform["01"])
-	end
-	os.sleep(utils.tick)
+	station.arrival_train_brake(arrival, platform_id)
+	station.platform_wait_for_train(platform.AD["end"], platform_id)
+	log.pass(station.name..": Train successfully arrived at platform "..platform_id)
+	station.platform_wait_for_departure(platform, platform_id)
 end
 
-conf.arrival["01"].AD.setTag("01-arrived")
+function	station.run()
+	station.running = true
 
+	while station.running do
+		local	info = station.conf.arrival["01"].AD.info()
+
+		if info then
+			station.train_arrival(station.conf.arrival["01"], "01")
+		end
+		os.sleep(utils.tick)
+	end
+end
 
 --- MAIN
 
+station.init()
+station.run()
